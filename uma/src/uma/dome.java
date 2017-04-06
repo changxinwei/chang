390a1c460c9fc2cbec02d8a0e8b7bcfb458a3de6
Your Name <you@example.com>
2017-04-06 16:37:03 +0800

    修改

---
+++ uma/src/uma/dome.java
@@ -0,0 +1,14 @@
+package uma;
+
+public class dome {
+
+	public dome() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

93adf2584534e381e8254651ba1a15721dc8b69b
Your Name <email@example.com>
2017-04-06 17:57:14 +0800

    修改

---
+++ uma/src/uma/Dome.java
@@ -0,0 +1,14 @@
+package uma;
+
+public class Dome {
+
+	public Dome() {
+	}
+
+	public static void main(String[] args) {
+	   Dome dome= new Dome();
+	   dome.getClass();
+	   System.out.println("................");
+	}
+
+}

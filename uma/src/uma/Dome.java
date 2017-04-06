1cfc8fec6d6c60d1c349bc2b012583829dffecd7
Your Name <email@example.com>
2017-04-06 21:57:00 +0800

    qqqq

--- uma/src/uma/Dome.java
+++ uma/src/uma/Dome.java
@@ -4,8 +4,6 @@ public class Dome {
 
 	public Dome() {
 	    System.out.println("3333333333333333");
-	    System.out.println("eeeeeeeeeee");
-	    System.out.println("eeeeeeeeeee");
 	}
 
 	public static void main(String[] args) {
